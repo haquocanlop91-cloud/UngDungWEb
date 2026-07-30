bee53d53cddebca0ff4da27589735b30a15fb39e
HAQUOCAN <haquocanlop91@gmail.com>
2026-07-30 15:48:03 +0700

    hi

--- java/2.js
+++ java/2.js
@@ -31,14 +31,14 @@ function showDate(){
     var m = date_now.getMonth() + 1;
     var t = date_now.getDay() + 1;
     var day = date_now.getDate();
-    document.getElementById("date").innerHTML = "Hom nay, " + (t == 1 ? "Chu Nhat" : "thu" + t) + "ngay" + day + "thang" + m + " nam " + y;
+    document.getElementById("date").innerHTML = "Hom nay, " + (t == 1 ? "Chu Nhat" : " thu " + t) + " ngay " + day + " thang " + m + " nam " + year;
     setInterval(showTime,1000);
 }
-function showTime(d){
+function showTime(){
     var date_now = new Date();
     var h = date_now.getHours();
     var m = date_now.getMinutes();
     var s = date_now.getSeconds();
-    document.getElementById("time").innerHTML = h + ":" + (m < 10 ? "0" + m : m) + ":" (s < 10 ? "0" + s : s);
+    document.getElementById("time").innerHTML = h + ":" + (m < 10 ? "0" + m : m) + ":" + (s < 10 ? "0" + s : s);
 
 }
