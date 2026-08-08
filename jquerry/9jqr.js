96f0b8766f49f63f00f71ac306879fa349ab9b1d
HAQUOCAN <haquocanlop91@gmail.com>
2026-08-08 21:56:39 +0700

    dkfk

---
+++ jquerry/9jqr.js
@@ -0,0 +1,7 @@
+var canvas = document.getElementById("can");
+var ctx = canvas.getContext("2d");
+var wwidth = window.innerWidth;
+var hheight = window.innerHeight;
+canvas.height = hheight;
+canvas.width = wwidth;
+canvas.style.background = "red";
